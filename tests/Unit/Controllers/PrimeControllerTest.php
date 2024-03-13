6cb9f162f1f2219d8de0b3b8069cf642f530e34f
Lucas Rocha <lucas_srocha@hotmail.com>
2024-03-13 19:35:20 +0000

    Unit Tests for Prime Controller

---
+++ tests/Unit/Controllers/PrimeControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests\Unit\Controllers;
+
+use App\Controllers\PrimeController;
+use App\Entities\Number;
+use App\Services\PrimeNumberService;
+use Mockery;
+use Mockery\Mock;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class PrimeControllerTest extends TestCase
+{
+    private PrimeController $primeController;
+    private PrimeNumberService|Mock $primeNumberService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->primeNumberService = Mockery::mock(PrimeNumberService::class);
+        $this->primeController = Mockery::mock(PrimeController::class, [$this->primeNumberService])->makePartial();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        unset($this->primeController);
+        unset($this->primeNumberService);
+    }
+
+
+    public function testIndex(): void
+    {
+        $expectedNumbers = [
+            ['value' => 1, 'isPrime' => false, 'multiples' => [1]],
+            ['value' => 2, 'isPrime' => true, 'multiples' => [1, 2]]
+        ];
+
+        $this->primeNumberService
+            ->shouldReceive('analyzeNumberRange')
+            ->andReturn(array_map(function($numberData) {
+                return new Number($numberData['value']);
+            }, $expectedNumbers));
+
+        $this->primeController
+            ->shouldReceive('index')
+            ->andReturn(new Response(''));
+
+        $response = $this->primeController->index();
+
+        $this->assertInstanceOf(Response::class, $response);
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+    }
+
+}
