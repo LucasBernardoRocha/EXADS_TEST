a9e429b9d27ccffd19bbdde0a936fc9bb50e3ac5
Lucas Rocha <lucas_srocha@hotmail.com>
2024-03-13 18:20:14 +0000

    Unit Tests for PrimeNumberService

--- src/Services/PrimeNumberService.php
+++ src/Services/PrimeNumberService.php
@@ -8,6 +8,10 @@ class PrimeNumberService
 {
     public function analyzeNumberRange(int $first = 1, int $last = 100): array
     {
+        if ($first > $last) {
+            throw new \InvalidArgumentException('The first number cannot be greater than the last number.');
+        }
+
         $results = [];
         for ($i = $first; $i <= $last; $i++) {
             $results[] = new Number($i);
---
+++ tests/Unit/Services/PrimeNumberServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Unit\Services;
+
+use App\Entities\Number;
+use App\Services\PrimeNumberService;
+use PHPUnit\Framework\TestCase;
+
+class PrimeNumberServiceTest extends TestCase
+{
+    private PrimeNumberService $service;
+
+    protected function setUp(): void
+    {
+        $this->service = new PrimeNumberService();
+    }
+
+    public function testAnalyzeNumberRangeWithDefaultRange(): void
+    {
+        $results = $this->service->analyzeNumberRange();
+
+        $this->assertCount(100, $results);
+        $this->assertInstanceOf(Number::class, $results[0]);
+        $this->assertEquals(1, $results[0]->getValue());
+    }
+
+    public function testAnalyzeNumberRangeWithCustomRange(): void
+    {
+        $results = $this->service->analyzeNumberRange(50, 75);
+
+        $this->assertCount(26, $results);
+        $this->assertInstanceOf(Number::class, $results[0]);
+        $this->assertEquals(50, $results[0]->getValue());
+        $this->assertEquals(75, $results[25]->getValue());
+    }
+
+    public function testAnalyzeNumberRangeWithReversedRange(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->service->analyzeNumberRange(10, 5);
+    }
+}
